04393532baea8d084a1830785cb385ae045fb285
Piotr Chmolowski <piotr@chmolowski.pl>
2011-08-10 20:27:51 +0200

    Customize aliases

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,12 +1,13 @@
-alias r='rvm use 1.8.7'
-
 alias f='RAILS_ENV=fi'
 
-alias sc='script/console'
-alias ss='script/server -p `available_rails_port`'
-alias sg='script/generate'
-alias sd='script/destroy'
+alias rg='bundle exec rails generate'
+alias rsv='bundle exec rails server -p `available_rails_port`'
+alias rc='bundle exec rails console'
+
+alias be='bundle exec'
+alias bi='bundle install'
+alias bu='bundle update'
 
 alias migrate='rake db:migrate db:test:clone'
 
-alias s="ps aux | grep \"[r]uby\" | grep script/server || echo \"You're not running any, dawg.\""
+alias s="ps aux | grep \"[s]cript/rails\" || echo \"You're not running any, dawg.\""
