5148baa83f43b8f57a3d46f0c0c55d2cc015d626
Piotr Chmolowski <piotr@chmolowski.pl>
2011-12-02 18:18:09 +0100

    Display user & computer name in prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -82,7 +82,7 @@ directory_name(){
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rbenv_prompt) in $(directory_name) $(project_name_color)$(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n%{$fg_bold[white]%}[$(whoami)@$(networksetup -getcomputername)]%{$reset_color%} $(rbenv_prompt) in $(directory_name) $(project_name_color)$(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[grey]%}$(todo)%{$reset_color%}"
 }
