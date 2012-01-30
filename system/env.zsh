c9573ffef5ec4283c357af7c59c00048779584f8
Piotr Chmolowski <piotr@chmolowski.pl>
2012-01-30 15:14:56 +0100

    chpwd() hook for loading environment variables

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,9 @@
 export EDITOR='mate -w'
+
+# Load environment variables from .env file if it exists
+# in the current directory
+function chpwd() {
+  if [ -r $PWD/.env ]; then
+    source $PWD/.env
+  fi
+}
