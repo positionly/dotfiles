45ab4e0e651ea292882031caf26936871ba952bb
Piotr Chmolowski <piotr@chmolowski.pl>
2012-03-07 22:09:31 +0100

    Register chpwd hook rather than defining chpwd

--- system/env.zsh
+++ system/env.zsh
@@ -2,8 +2,13 @@ export EDITOR='mate -w'
 
 # Load environment variables from .env file if it exists
 # in the current directory
-function chpwd() {
+function load_local_env() {
   if [ -r $PWD/.env ]; then
     source $PWD/.env
   fi
 }
+
+# Register the function so it is called whenever the working
+# directory changes.
+autoload add-zsh-hook
+add-zsh-hook chpwd load_local_env
