a313d479f054f1eb7b8115942ba469cc9072decd
Piotr Chmolowski <piotr@chmolowski.pl>
2012-06-19 15:06:10 +0200

    Add some colors & formatting to git log

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -8,5 +8,6 @@ alias gca='git commit -v -a'
 alias gb='git branch'
 alias gba='git branch -a'
 
-
+# source: http://coderwall.com/p/euwpig?i=3&p=1&t=git
+alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --"
 
