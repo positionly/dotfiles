b1d17937cf9f2095f04d296f04690bbfb1aa7119
Piotr Chmolowski <piotr@chmolowski.pl>
2013-09-11 10:54:26 +0200

    Set sublime as the default editor

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -2,7 +2,7 @@
 alias gst='git status'
 alias gl='git pull'
 alias gp='git push'
-alias gd='git diff | mate'
+alias gd='git diff | subl'
 alias gc='git commit -v'
 alias gca='git commit -v -a'
 alias gb='git branch'
